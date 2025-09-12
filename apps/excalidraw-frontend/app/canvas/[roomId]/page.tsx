8523356cca41a5ceb5975a38e36b3f388100351a
itsnaved <itsnaved11@gmail.com>
2025-09-12 16:12:24 +0530

    Added Rectangle shape

--- apps/excalidraw-frontend/app/canvas/[roomId]/page.tsx
+++ apps/excalidraw-frontend/app/canvas/[roomId]/page.tsx
@@ -1,4 +1,5 @@
 "use client"
+import { initDraw } from "@/draw";
 import { useEffect, useRef } from "react"
 
 export default function Canvas(){
@@ -9,40 +10,15 @@ export default function Canvas(){
             const canvas= canvasRef.current;
             const ctx= canvas.getContext("2d");
 
-            if(!ctx){  
-                return;
+            if(canvasRef.current){  
+                initDraw(canvasRef.current);
             }
 
-            let clicked= false; 
-            let startX= 0;
-            let startY= 0;
-
-            canvas.addEventListener('mousedown', (e)=>{
-                clicked= true;
-                startX= e.clientX;
-                startY= e.clientY;
-
-            })
-
-            canvas.addEventListener('mouseup', (e)=>{
-                clicked= false;
-                console.log(e.clientX);
-                console.log(e.clientY);
-            })
-
-            canvas.addEventListener('mousemove', (e)=>{
-                if(clicked){
-                    const width= e.clientX - startX;
-                    const height= e.clientY - startY;
-                    ctx.clearRect(0, 0, canvas.width, canvas.height);
-                    ctx.strokeRect(startX, startY, width, height);
-                }
-            })
         }
     },[canvasRef])
 
 
     return <div>
-        <canvas ref={canvasRef} width={500} height={500}></canvas>
+        <canvas ref={canvasRef} width={2000} height={1080}></canvas>
     </div>
 }
